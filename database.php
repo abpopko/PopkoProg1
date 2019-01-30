bf64b3a83e8369061fdc6a0da3daf715b0dd5310
abpopko <47129962+abpopko@users.noreply.github.com>
2019-01-29 16:18:41 -0800

    Update database.php

--- database.php
+++ database.php
@@ -3,10 +3,10 @@ class Database
 {
 	// these are not the real passwords
 	// for real passwords, see file in ../database subdirectory
-	private static $dbName = 'crud_tutorial' ; 
-	private static $dbHost = 'localhost' ;
-	private static $dbUsername = 'root';
-	private static $dbUserPassword = '';
+	private static $dbName = 'abpopko355wi19' ; 
+	private static $dbHost = '10.8.30.49' ;
+	private static $dbUsername = 'abpopko355wi19';
+	private static $dbUserPassword = 'Nuggets1';
 	
 	private static $cont  = null;
 	
